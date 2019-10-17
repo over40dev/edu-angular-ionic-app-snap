314d7f2597f51ddbd7a33fca6406fd3c66d631b8
Cogentx <cogentx@live.com>
2019-10-17 21:24:58 -0230

    snap-L03-home page layout - view html (w)

--- src/app/slideshow/slideshow.page.ts
+++ src/app/slideshow/slideshow.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-slideshow',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideshowPage implements OnInit {
 
+  public imageSrc: SafeResourceUrl;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  playPhotos() {
+
+  }
+
+  close() {
+    
   }
 
 }
